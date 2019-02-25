471a47e8608bc147f8596fe6e4bce55dce006d03
amon cruz <amoncruz13@gmail.com>
2019-02-24 21:08:01 -0300

    icons

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -17,7 +17,7 @@
                         <div class="title"><?php the_title(); ?>
                     <div class="icons"><i class="fab fa-facebook" style="color:blue;"></i>
                     <i class="fab fa-whatsapp" style="color:green"></i>
-                    <i class="fab fa-linkedin" style="color:blue"></i>
+                    <i class="fab fa-linkedin" style="color:#0274b3"></i>
                     <i class="fab fa-instagram" style="color:purple"></i></div></div>
                         <h1 style="display: none;"><?php the_title(); ?></h1>
                         <article>
