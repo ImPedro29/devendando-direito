e35797c79709c154b08adbdbd7560e85dbdb4b01
amon cruz <amoncruz13@gmail.com>
2019-03-07 14:36:45 -0300

    propaganda

--- themes/mp-theme/sidebar.php
+++ themes/mp-theme/sidebar.php
@@ -1,6 +1,13 @@
 <div class="aside" id="aside">
 <div class="propaganda">
         <div class="title">Propaganda</div>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7690247183685353",
+    enable_page_level_ads: true
+  });
+</script>
     </div>
     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
 
