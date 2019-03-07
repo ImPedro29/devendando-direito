905136db6a862721160ecd44277a0e03049402cd
impedro29 <pi3virgula14@outlook.com>
2019-03-07 20:26:50 -0300

    Arrumando propaganda

--- themes/mp-theme/sidebar.php
+++ themes/mp-theme/sidebar.php
@@ -1,16 +1,10 @@
 <div class="aside" id="aside">
-<div class="propaganda">
-        <div class="title">Propaganda</div>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-7690247183685353",
-    enable_page_level_ads: true
-  });
-</script>
-    </div>
     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
 
     <?php endif; ?>
-   
+    <script>
+        let widget = document.querySelector(".widget");
+        widget.classList.add("propaganda");
+        widget.removeChild(widget.querySelector(".title"));
+    </script>
 </div>
