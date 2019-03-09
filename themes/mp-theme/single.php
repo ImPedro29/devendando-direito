1e9996024f74834363e5ec3b25fffaea94167a6a
amon cruz <amoncruz13@gmail.com>
2019-03-09 01:11:15 -0300

    adwa

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -14,7 +14,7 @@
                         <ul class="post-categories">
                             <li>
                             <?php
-                 echo (string) get_category_parents( $cat, true, ' &raquo; ' ); 
+                 
                            
                             ?>
                             </li>
