5867f224a3ee19196dfe4126d9f62372cdb47eac
amon cruz <amoncruz13@gmail.com>
2019-03-09 01:03:12 -0300

    dad

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -14,7 +14,7 @@
                         <ul class="post-categories">
                             <li>
                             <?php
-                 echo get_category_parents( $cat, true, ' &raquo; ' ); 
+                 echo strtolower(get_category_parents( $cat, true, ' &raquo; ' )); 
                            
                             ?>
                             </li>
