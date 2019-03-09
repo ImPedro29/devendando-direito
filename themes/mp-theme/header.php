631dcc6102ad947fd701bf5aed36e1b33998b039
impedro29 <pi3virgula14@outlook.com>
2019-03-09 09:21:29 -0300

    Adsense

--- themes/mp-theme/header.php
+++ themes/mp-theme/header.php
@@ -40,3 +40,11 @@
         </div>
     </div>
 </header>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-5041420412525265",
+        enable_page_level_ads: true
+    });
+</script>
