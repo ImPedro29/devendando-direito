297cf03fd03d36a72065de4627681851b59fd40a
amon cruz <amoncruz13@gmail.com>
2019-03-10 18:41:39 -0300

    going back

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -17,10 +17,7 @@
                $category_detail=get_the_category($post->ID);//$post->ID
                foreach($category_detail as $cd){
                    
-                   ?><a href="<?php echo "/".strtolower($cd->cat_name);
-                   if($cd->cat_name=="SIGNIFICADO"){
-                   echo "https://desvendandodireito.com.br/category/significados/";
-                }?>"><?php
+                   ?><a href="<?php echo "/".$cd->cat_name ?>"><?php
                echo $cd->cat_name." > ";
               
                }?>
