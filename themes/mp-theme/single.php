e49313a70ea9e2d9eaebd9c29759aadf22522e2a
amon cruz <amoncruz13@gmail.com>
2019-03-10 19:30:45 -0300

    :D

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -25,7 +25,7 @@
                         echo $cd->cat_name." > ";
                         
                     }else{
-                   ?><a href="<?php echo "/".strtolower($cd->cat_name); ?>"><?php
+                   ?><a href="<?php echo "/category/significados/".strtolower($cd->cat_name); ?>"><?php
                    echo $cd->cat_name." > ";
                     }
               
