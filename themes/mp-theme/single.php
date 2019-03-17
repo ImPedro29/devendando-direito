ca7994a678fac0788a2967218a99235ac0476e3a
amon cruz <amoncruz13@gmail.com>
2019-03-17 14:19:20 -0300

    test

--- themes/mp-theme/single.php
+++ themes/mp-theme/single.php
@@ -42,7 +42,7 @@
                    
                     }
               print_r($arrayCategory);
-              echo " o tamanho é:".$arrayCategory.length();
+              echo " o tamanho é:".sizeof($arrayCategory);
                }?>
                </a>
 
