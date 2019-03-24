5d7560a3298107e6573385687345a40ec5df3e8d
impedro29 <pi3virgula14@outlook.com>
2019-03-24 17:45:30 -0300

    Se n tiver imagem n aparece

--- themes/mp-theme/archive.php
+++ themes/mp-theme/archive.php
@@ -28,7 +28,7 @@
                         ?>
                         <a href="<?php the_permalink();?>">
                             <div class="posts">
-                                <?php $thumb = get_the_post_thumbnail_url(); if(!isset($thumb)){ echo $thumb; ?><div class="img"><img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="" /></div> <?php } ?>
+                                <?php if(has_post_thumbnail(get_the_ID())){ ?><div class="img"><img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="" /></div> <?php } ?>
                                 <div class="conteudo">
                                     <h3><?php the_title(); ?></h3>
                                     <article>
