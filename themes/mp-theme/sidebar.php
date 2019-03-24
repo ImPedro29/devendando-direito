9877e0ffe6248457e6e49f4dd2312bf31294a455
impedro29 <pi3virgula14@outlook.com>
2019-03-24 13:52:47 -0300

    Arrumando sidebar.php

--- themes/mp-theme/sidebar.php
+++ themes/mp-theme/sidebar.php
@@ -1,12 +1,13 @@
 <div class="aside" id="aside">
     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
-        <script>
-            let widget = document.querySelector(".widget");
-            if(widget.querySelector(".title").innerHTML === "[ATIVA]" || widget.querySelector(".title").innerHTML === "[ATIVO]"){
-                let adsensecode = widget.querySelector("p").innerHTML;
-                document.getElementById("aside").removeChild(widget);
-                document.body.innerHTML += adsensecode;
-            }
-        </script>
+
     <?php endif; ?>
+    <script>
+        let widget = document.querySelector(".widget");
+        if(widget.querySelector(".title").innerHTML === "[ATIVA]" || widget.querySelector(".title").innerHTML === "[ATIVO]"){
+            let adsensecode = widget.querySelector("p").innerHTML;
+            document.getElementById("aside").removeChild(widget);
+            document.body.innerHTML += adsensecode;
+        }
+    </script>
 </div>
